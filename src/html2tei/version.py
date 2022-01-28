66b404badd5f07a674a19b145c2d43fc84e40f29
Indig Balázs <dlazesz@users.noreply.github.com>
2022-01-28 15:29:53 +0100

    Fix imports to enable easier development of the package without rebuilding (#48)

--- src/html2tei/version.py
+++ src/html2tei/version.py
@@ -3,7 +3,10 @@
 
 from importlib import metadata
 
-__version__ = metadata.version('html2tei')
+try:
+    __version__ = metadata.version('html2tei')
+except ModuleNotFoundError:
+    __version__ = 'THIS IS NOT A PACKAGE!'
 
 
 if __name__ == '__main__':
