1b2a77e72257704e1a54befdc3d6164537d69ad8
sarkozizsofia <53250113+sarkozizsofia@users.noreply.github.com>
2022-03-07 19:52:31 +0100

    p444 test (#62)
    * p444 test
    
    * links
    
    * small fix
    
    * dicts updated
    
    * .
    
    * .
    
    * p444 finish

--- configs/p444/p444_specific.py
+++ configs/p444/p444_specific.py
@@ -4,63 +4,51 @@
 import re
 
 from bs4 import BeautifulSoup
+from os.path import join as os_path_join, dirname as os_path_dirname, abspath as os_path_abspath
 
-from html2tei import parse_date, decompose_listed_subtrees_and_mark_media_descendants, tei_defaultdict
+from src.html2tei import parse_date, decompose_listed_subtrees_and_mark_media_descendants, tei_defaultdict, BASIC_LINK_ATTRS
 
 PORTAL_URL_PREFIX = 'https://444.hu/'
-ARTICLE_ROOT_PARAMS_SPEC = [(('main',), {'id': 'content-main'})]  # ,(('section',), {'id': 'main-section'})]
+ARTICLE_ROOT_PARAMS_SPEC = [(('section',), {'id': 'main-section'})]
+# (('main',), {'id': 'content-main'})]
 
 HIGHLIGHT = re.compile(r'.*highlight.*')
 A_TAGS = {'a', '0_MDESC_a'}
-
-# https://444.hu/2015/03/15/24-ora-az-uj-kozmediaval?page=4  más szerkezetű cikk
-# https://444.hu/2016/06/15/oroszorszag-szlovakia-elo?page=3 közvetítés author, section
-# https://444.hu/2016/02/10/ejjel-erkezem-mondta-a-ciklon  üresnek írja de nem az jelenleg
-# https://444.hu/2014/05/10/orban-viktor-eskuszik-elo?page=2
-# https://444.hu/2021/02/18/mar-megint-24-ora-a-kozmediaval?page=6
 
 
 def get_meta_from_articles_spec(tei_logger, url, bs):
     data = tei_defaultdict()
     data['sch:url'] = url
-    basic_article = True
+    report_dates_cont = []
+
     raw_meta = bs.find('div', {'id': 'headline'})
-    articles = bs.find_all('article')  # TODO
-    report = bs.find_all('article', {'class': 'report', 'data-content-id': True})
+    title = bs.find('meta', {'name': 'title'})
+    if title is not None:
+        data['sch:name'] = title.text.strip()
+    else:
+        tei_logger.log('WARNING', f'{url}: TITLE NOT FOUND!')
+
     if raw_meta is not None:
-        """if len(articles) > 1 and all('class' in art.attrs.keys() and art.attrs['class'] == ['report']
-                                     for art in articles):
-            basic_article = False"""
-        if len(report) > 1:
-            print('report', url)
-            basic_article = False
+        basic_article = False
     else:
         tei_logger.log('WARNING', f'{url}: ARTICLE BODY NOT FOUND OR UNKNOWN ARTICLE SCHEME!')
         return None
-
     if basic_article in [True, False]:
-        date_tag = bs.find('meta', property='article:published_time')
-        # 2021-05-11T19:31:11+02:00"
+        date_tag = bs.find('meta', property='article:published_time')  # 2021-05-11T19:31:11+02:00"
         if date_tag is not None:
             parsed_date = parse_date(date_tag.attrs['content'][:19], '%Y-%m-%dT%H:%M:%S')
-            data['sch:datePublished'] = parsed_date
-        else:
-            tei_logger.log('WARNING', f'{url}: DATE NOT FOUND IN URL!')
+            report_dates_cont.append(parsed_date)
+
         modified_date = bs.find('meta', property='article:modified_time').attrs['content']
         if modified_date is not None:
             parsed_moddate = parse_date(modified_date[:19], '%Y-%m-%dT%H:%M:%S')
-            data['sch:dateModified'] = parsed_moddate
+            report_dates_cont.append(parsed_moddate)
         lead_tag = bs.find('meta', {'name': 'description'})
         if lead_tag is not None:
             lead_text = lead_tag.attrs['content'].strip()
             data['sch:abstract'] = lead_text
 
     if basic_article is True:
-        title = raw_meta.find('h1')
-        if title is not None:
-            data['sch:name'] = title.text.strip()
-        else:
-            tei_logger.log('WARNING', f'{url}: TITLE TAG NOT FOUND!')
         authors_list = raw_meta.find(class_='byline__authors')
         if authors_list is not None:
             authors_list = [a.text.strip() for a in authors_list.find_all('a')]
@@ -76,26 +64,59 @@ def get_meta_from_articles_spec(tei_logger, url, bs):
         if keywords_root is not None:
             keywords_list = keywords_root.attrs['content'].split(',')
             data['sch:keywords'] = keywords_list
+        else:
+            tei_logger.log('DEBUG', f'{url}: KEYWORDS NOT FOUND!')
         lead_tag = bs.find('meta', {'name': 'description'})
         if lead_tag is not None:
             lead_text = lead_tag.attrs['content'].strip()
             data['sch:abstract'] = lead_text
-        return data
+
     elif not basic_article:
+        # https://444.hu/2015/11/16/orban-viktor-europat-megtamadtak-elo
+        report_dates = bs.find_all('a', class_='report__date')
+        if report_dates:
+            if report_dates[0].find('div', class_='report__time'):  # 2016. 06. 14., kedd
+                for one_date_tag in report_dates:
+                    datetime_str = ''
+                    for d in one_date_tag.contents:
+                        d_str = d.text.strip()
+                        if len(d_str) == 5:
+                            datetime_str = d_str
+                        elif len(d_str) > 5:
+                            datetime_str = f'{d_str[0:13]} {datetime_str}'
+                    parsed_date = parse_date(datetime_str, '%Y. %m. %d. %H:%M')
+                    if parsed_date:
+                        report_dates_cont.append(parsed_date)
+            else:
+                for rep in report_dates:    # '2014. október. 26., vasárnap, 19:57'
+                    rep_span = rep.find('span')
+                    if rep_span is not None:
+                        parsed_rep_datetime = parse_date(rep_span.text.strip(), '%Y. %B. %d., %A, %H:%M')
+                        if parsed_rep_datetime is not None:
+                            report_dates_cont.append(parsed_rep_datetime)
+
         # The scheme of broadcasts is different, metadata is handled differently
         authors_tag = bs.find_all(class_='report__author')
         if authors_tag is not None:
             authors_list = [au.text.strip() for au in authors_tag]
             data['sch:author'] = authors_list
+        else:
+            authors_list = raw_meta.find_all(class_='byline__authors')
+            if len(authors_list) > 0:
+                authors_list = [a.find('a').text.strip() for a in authors_list]
+                data['sch:author'] = authors_list
+            else:
+                tei_logger.log('WARNING', f'{url}: AUTHOR TAG NOT FOUND!')
         section = bs.find('meta', {'itemprop': 'articleSection'})
         if section is not None and 'content' in section.attrs.keys():
             data['sch:articleSection'] = section.attrs['content'].strip()
-        title = bs.find('h1', {'class': 'livestream__title'})
-        if title is not None:
-            data['sch:name'] = title.text.strip()
-    return data
+    if len(report_dates_cont) > 0:
+        data['sch:datePublished'] = min(report_dates_cont)
+        data['sch:dateModified'] = max(report_dates_cont)
+    else:
+        tei_logger.log('WARNING', f'{url}: DATE NOT FOUND IN URL!')
 
-# https://444.hu/2016/02/10/ejjel-erkezem-mondta-a-ciklon scripttel töltődik ki a cikktörzs szövege
+    return data
 
 
 def excluded_tags_spec(tag):
@@ -111,12 +132,11 @@ def excluded_tags_spec(tag):
 
 BLOCK_RULES_SPEC = {}
 BIGRAM_RULES_SPEC = {'szakasz': {('temp_table_id', 'det_by_child'): ('table_text', 'temp')}}
-# kozvetites meta: span > kozvetites_ido
 
-LINKS_SPEC = {'a': 'href', '0_MDESC_a': 'href', 'img': 'href', '0_MDESC_img': 'href'}
+LINKS_SPEC = BASIC_LINK_ATTRS
 DECOMP = [(('div',), {'id': 'headline'}),
           (('div',), {'class': 'hide-print'}),
-          (('div',), {'class': 'hide-for-print'}),  # class=row hide-for-print
+          (('div',), {'class': 'hide-for-print'}),
           (('aside',), {'id': 'content-sidebar'}),
           (('div',), {'id': 'ep-banner'}),
           (('div',), {'class': 'widget-recommendation'}),
@@ -152,20 +172,17 @@ DECOMP = [(('div',), {'id': 'headline'}),
           (('div',), {'class': 'pagination'}),
           (('div',), {'class': 'livestream__featured-list'}),
           (('div',), {'class': 'show-md'}),
-          (('a',), {'class': 'pr-box'}),    #??
-
+          (('a',), {'class': 'pr-box'}),  # ??
+          (('div',), {'id': 'mc_embed_signup'})
           ]
-# <aside data-load-type=noop data-panelmod-type=relatedContent data-role=@STYLE>
-# <a class=pr-box pr-box--compact pr-box--centered href=@LINK>
-# 'tovább'-os, kellhet https://444.hu/2014/09/15/orban-viktor-visszaadja-a-bankok-penzet-az-embereknek
-#<div class=row between-xs livestream>
-MEDIA_LIST = [(('div',), {'id': 'bodyContent'}),  # 1 db wikipedia cikk
+
+MEDIA_LIST = [(('div',), {'id': 'bodyContent'}),  # 1 wikipedia cikk
               (('div',), {'id': 'mw-content-text'}),
               (('figure',), {}),
               (('iframe',), {}),
-              (('object',), {}),  # ????
-              (('video',), {}),  # ????
-              (('div',), {'class': 'embedly-card'}),  # ????
+              (('object',), {}),
+              (('video',), {}),
+              (('div',), {'class': 'embedly-card'}),
               (('div',), {'class': 'fb-video'}),
               (('div',), {'class': 'fb-post'}),
               (('blockquote',), {'class': 'twitter-tweet'}),
@@ -178,20 +195,33 @@ MEDIA_LIST = [(('div',), {'id': 'bodyContent'}),  # 1 db wikipedia cikk
 
 
 def decompose_spec(article_dec):
-    # from 2020: <a class="pr-box pr-box--compact pr-box--centered" href="https://membership.444.hu">
+    # after 2020: <a class="pr-box pr-box--compact pr-box--centered" href="https://membership.444.hu">
     for h2 in article_dec.find_all('h2'):
         for a in h2.find_all('a', {'href': 'direkt36_spec'}):
             print(h2)
             a.decompose()
     decompose_listed_subtrees_and_mark_media_descendants(article_dec, DECOMP, MEDIA_LIST)
-
     return article_dec
 
 
-BLACKLIST_SPEC = ['https://444.hu/2021/02/19/az-szfe-s-hallgatok-tobb-mint-otode-passzivaltatott-lehet-hogy-kesobb-vissza-se-mehetnek-a-kepzesukbe']
-LINK_FILTER_SUBSTRINGS_SPEC = re.compile('|'.join(['LINK_FILTER_DUMMY_STRING']))
+BLACKLIST_SPEC = [
+    'https://444.hu/2021/02/19/az-szfe-s-hallgatok-tobb-mint-otode-passzivaltatott-lehet-hogy-kesobb-vissza-se'
+    '-mehetnek-a-kepzesukbe',
+    'https://444.hu/2015/05/23/amerika-ezentul-nem-gyujtheti-tomegesen-a-telefonhivasok-metaadatait',
+    'https://444.hu/2015/05/23/kelet-europaban-nyitna-uj-gyarat-a-land-rover',
+    'https://444.hu/2015/05/23/sajatjanak-hazudott-juhnyaj-utan-vett-fel-agrartamogatast-a-bekolcei-agrarbunozo'] + \
+                 [url.strip() for url in
+                  open(os_path_join(os_path_dirname(os_path_abspath(__file__)), '444_EMPTY_ARTICLES.txt')).readlines()]
 
-MULTIPAGE_URL_END = re.compile(r'.*\?page=.*')  # Dummy
+LINK_FILTER_SUBSTRINGS_SPEC = re.compile('|'.join(['http://Az%20Index%20egy%C3%A9bk%C3%A9nt%20ezeket%20a%20k%C3%A9rd%C3%A9seket%20k%C3%BCldte%20el%20kedden%20az%20operat%C3%ADv%20t%C3%B6rzsnek:%20%20A%20s%C3%BCrg%C5%91ss%C3%A9gi%20ell%C3%A1t%C3%A1s%20%C3%BAj%20protokollja%20szerint%20csontt%C3%B6r%C3%A9sek%20eset%C3%A9n%20csak%20ny%C3%ADlt%20t%C3%B6r%C3%A9sekn%C3%A9l%20%C3%A9s%20s%C3%BAlyos%20esetekben%20v%C3%A9geznek%20helyre%C3%A1ll%C3%ADt%C3%B3%20m%C5%B1t%C3%A9teket.%20Jelenleg%20teh%C3%A1t%20%22sima%22%20v%C3%A9gtagt%C3%B6r%C3%A9s%20eset%C3%A9n%20nincs%20helyre%C3%A1ll%C3%ADt%C3%B3%20m%C5%B1t%C3%A9t?%20Mi%20sz%C3%A1m%C3%ADt%20s%C3%BAlyosnak?%20Nem%20okoz%20ez%20maradand%C3%B3%20eg%C3%A9szs%C3%A9groml%C3%A1st?%20Fenntartj%C3%A1k,%20hogy%20nem%20javasolj%C3%A1k%20a%20maszkvisel%C3%A9st%20mindenkinek,%20f%C5%91k%C3%A9nt%20azok%20ut%C3%A1n,%20hogy%20a%20minisztereln%C3%B6k%20a%20parlamentben%20azt%20mondta,%20val%C3%B3j%C3%A1ban%20mindenkinek%20hordania%20kellene,%20de%20nincs%20el%C3%A9g%20ebb%C5%91l%20Magyarorsz%C3%A1gon,%20nem%20tudj%C3%A1k%20biztos%C3%ADtani%20a%20lakoss%C3%A1gnak,%20ez%C3%A9rt%20nem%20lehet%20k%C3%B6telez%C5%91v%C3%A9%20tenni?%20A%20t%C3%B6bb%20mint%2013%20ezer%20magyarorsz%C3%A1gi%20mintav%C3%A9tel%20pontosan%20h%C3%A1ny%20emberen%20t%C3%B6rt%C3%A9nt%20meg?%20Konkr%C3%A9tan%20h%C3%A1ny%20embert%20teszteltek%20m%C3%A1r%20Magyarorsz%C3%A1gon%20a%20hat%C3%B3s%C3%A1gok%20az%20%C3%BAj%20koronav%C3%ADrussal,%20%C3%A9s%20egy%20esetben%20h%C3%A1nyszor%20kell%20mint%C3%A1t%20venni?%20%C3%81tlagosan%20napi%20ezer%20elv%C3%A9gzett%20mintav%C3%A9telt%20t%C3%BCntetnek%20fel,%20legal%C3%A1bbis%20az%20elm%C3%BAlt%20egy%20h%C3%A9tben,%20a%20kijel%C3%B6lt%20%C3%BAj%20laborokkal.%20Pontosan%20naponta%20h%C3%A1ny%20labortesztet%20v%C3%A9geznek%20a%20NNK%20k%C3%B6zponti%20laborj%C3%A1ban,%20%C3%A9s%20h%C3%A1ny%20darab%20mintav%C3%A9telt%20%C3%A9rt%C3%A9kelnek%20ki%20a%20kijel%C3%B6lt%207%20laborban?%20Mennyivel%20t%C3%B6bb%20vagy%20kevesebb%20a%20k%C3%B3rh%C3%A1zakban%20s%C3%BAlyos%20t%C3%BCd%C5%91gyullad%C3%A1ssal%20kezeltek%20sz%C3%A1ma%20az%20%C3%A9vnek%20ebben%20a%20szakasz%C3%A1ban%20megszokott%20%C3%A1tlagos%20adatokhoz%20k%C3%A9pest?%20Minden%20t%C3%BCd%C5%91gyullad%C3%A1sos%20t%C3%BCnettel,%20vagy%20%C3%A9pp%20konkr%C3%A9tan%20t%C3%BCd%C5%91gyullad%C3%A1ssal%20k%C3%B3rh%C3%A1zban%20fekv%C5%91%20betegn%C3%A9l%20elv%C3%A9gzik%20a%20koronav%C3%ADrus-tesztet%20a%20jelenlegi%20elj%C3%A1r%C3%A1srend%20szerint?%20N%C3%A1lunk%20nem%20volt%20kiugr%C3%B3%20n%C3%B6veked%C3%A9s%20a%20t%C3%BCd%C5%91gyullad%C3%A1sos,%20influenz%C3%A1s%20megbeteged%C3%A9sek%20sz%C3%A1m%C3%A1ban%20a%20koronav%C3%ADrus%20hivatalos%20megjelen%C3%A9se%20el%C5%91tt?',
+                                                   'http://Az',
+                                                   'https://www.w3.org/1999/xlink',
+                                                   'Fot',
+                                                   'mailto:<blockquote%20class=%22instagram-media%22%20data-instgrm'
+                                                   ]))
+
+#
+MULTIPAGE_URL_END = re.compile(r'.*(\?|&)page=.*')
 
 
 def next_page_of_article_spec(curr_html):
@@ -207,41 +237,3 @@ def next_page_of_article_spec(curr_html):
         return next_page_link2.attrs['href']
     return None
 
-#2022-01-25 18:05:21,171 WARNING: https://444.hu/2014/10/26/tuntetes-az-internetado-ellen-elo?reverse=1&page=3: AUTHOR TAG NOT FOUND!
-# https://444.hu/post/90b775c8c2f830d284c08ac379ba216b   body
-# https://444.hu/2015/05/23/gyere-boggie-gyere-europa-nem-veheti-el-toled-az-aranyermet?reverse=1&page=9 autjor
-
-# https://444.hu/2018/06/27/brazilia-eselyeshez-meltoan-daralta-be-es-ejtette-ki-a-szerbeket   <table ?
-#https://444.hu/2013/04/17/gyanusitott-lett-a-fideszes-kepviselo-fiat-segito-dekan   main
-# https://444.hu/2019/12/17/az-alapjogi-biztos-soron-kivuli-vizsgalatot-indit-a-gyori-gyerekgyilkossag-ugyebe <div class=pr-box__info>
-#https://444.hu/2015/11/16/orban-viktor-europat-megtamadtak-elo <main class=@smallNUM columns id=content-main>
-# 2022-01-11 17:26:31,543 WARNING: https://444.hu/2014/07/08/megallithatja-e-barki-a-gepezetet-2?reverse=1&page=3: AUTHOR TAG NOT FOUND!
-# 2022-01-11 17:26:31,543 WARNING: https://444.hu/2014/07/08/megallithatja-e-barki-a-gepezetet-2?reverse=1&page=3: SECTION TAG NOT FOUND!
-# 2022-01-11 17:25:20,937 ERROR: TEI validation error: https://444.hu/2014/07/17/lezuhant-egy-malaj-utasszallito-ukrajna-felett?reverse=1&page=2 50ea8216-4e96-5be0-9d59-eea71deb5554.xml Did not expect element p there, line 356
-"""Traceback (most recent call last):
-  File "/usr/lib/python3.8/runpy.py", line 194, in _run_module_as_main
-    return _run_code(code, main_globals, None,
-  File "/usr/lib/python3.8/runpy.py", line 87, in _run_code
-    exec(code, run_globals)
-  File "/home/eltedh/PycharmProjects/HTML2TEI/src/html2tei/__main__.py", line 129, in <module>
-    entrypoint()
-  File "/home/eltedh/PycharmProjects/HTML2TEI/src/html2tei/__main__.py", line 119, in entrypoint
-    run_main(*common_args[:-1], command_dict[command][0], args, logfile_level=common_args[-1],
-  File "/home/eltedh/PycharmProjects/HTML2TEI/src/html2tei/processing_utils.py", line 206, in run_main
-    for publish_date in run_fun(warc_level_params, log_file_names_and_modes, process_article_fun,
-  File "/home/eltedh/PycharmProjects/HTML2TEI/src/html2tei/processing_utils.py", line 118, in run_multiple_process
-    yield after_function(ret, after_params, fhandles)
-  File "/home/eltedh/PycharmProjects/HTML2TEI/src/html2tei/portal_article_cleaner.py", line 282, in after_clean
-    final_filename = validator_hasher_compressor.process_one_file(url, desired_filename, filename_suff, tei_string)
-  File "/home/eltedh/PycharmProjects/HTML2TEI/src/html2tei/validate_hash_zip.py", line 165, in process_one_file
-    xml_etree = etree.fromstring(raw_xml_str)
-  File "src/lxml/etree.pyx", line 3252, in lxml.etree.fromstring
-  File "src/lxml/parser.pxi", line 1912, in lxml.etree._parseMemoryDocument
-  File "src/lxml/parser.pxi", line 1800, in lxml.etree._parseDoc
-  File "src/lxml/parser.pxi", line 1141, in lxml.etree._BaseParser._parseDoc
-  File "src/lxml/parser.pxi", line 615, in lxml.etree._ParserContext._handleParseResultDoc
-  File "src/lxml/parser.pxi", line 725, in lxml.etree._handleParseResult
-  File "src/lxml/parser.pxi", line 654, in lxml.etree._raiseParseError
-  File "<string>", line 280
-lxml.etree.XMLSyntaxError: XML declaration allowed only at the start of the document, line 280, column 10"""
-# utolsó: 2022-01-25 20:02:04,912 WARNING: https://444.hu/2014/06/29/ma-egy-csapat-biztosan-tortenelmet-ir?reverse=1&page=6: AUTHOR TAG NOT FOUND!
