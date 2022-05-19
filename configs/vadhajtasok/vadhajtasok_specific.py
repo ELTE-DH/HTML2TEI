9725a43c491ad618c9f03cb3b47db80fe9f5b8f7
sarkozizsofia <53250113+sarkozizsofia@users.noreply.github.com>
2022-05-19 16:28:58 +0200

    Vadhajtasok tei (#73)
    * tags OK
    
    * in progress1
    
    * ok
    
    * TEI OK !
    
    * 'sch:availableLanguage'

--- configs/vadhajtasok/vadhajtasok_specific.py
+++ configs/vadhajtasok/vadhajtasok_specific.py
@@ -2,31 +2,34 @@
 # -*- coding: utf-8, vim: expandtab:ts=4 -*
 
 import re
+from langdetect import detect
 
 from os.path import join as os_path_join, dirname as os_path_dirname, abspath as os_path_abspath
-from html2tei import parse_date, BASIC_LINK_ATTRS, decompose_listed_subtrees_and_mark_media_descendants, tei_defaultdict
+from src.html2tei import parse_date, BASIC_LINK_ATTRS, decompose_listed_subtrees_and_mark_media_descendants,\
+    tei_defaultdict
 
 PORTAL_URL_PREFIX = 'https://www.vadhajtasok.hu/'
 
 ARTICLE_ROOT_PARAMS_SPEC = [(('div',), {'class': 'entry-content content-article'})]
 
-HTML_BASICS = {'p', 'h3', 'h2', 'h4', 'h5', 'em', 'i', 'b', 'strong', 'mark', 'u', 'sub', 'sup', 'del', 'strike',
-               'ul', 'ol', 'li', 'table', 'tr', 'td', 'th', 'quote', 'figure', 'iframe', 'script', 'noscript'}
 
-SOURCE_1 = ['Forrás:', 'Írta:']
-SOURCE_2 = ['Vadhajtasok.hu', 'Pestisracok.hu', '888.hu', 'MTI', 'Magyar Nemzet', 'HVG']
-# If we stay at the current solution (see the get_meta_from_articles_spec function) and use
-# both SOURCE_1 and SOURCE_2 lists, then we will be able to find the majority of the sources but not all of them.
-# Note: the SOURCE_2 list is not complete, can still be improved but with the current routine it is not necessary.
+# https://www.vadhajtasok.hu/2019/01/05/nagy-szrban-a-9-11-legendaja-hackerek-megszereztek-a-titkos-aktait
+SOURCE_2 = ['Vadhajtasok.hu', 'Pestisracok.hu', '888.hu', 'MTI', 'Magyar Nemzet', 'HVG', 'Demokrata', 'Index',
+            'Mandiner', 'Origo', 'Vadhajtások', 'Magyar Hírlap', 'hirado.hu', 'Magyar Hírlap', 'V4NA']
 
 
 def get_meta_from_articles_spec(tei_logger, url, bs):
     data = tei_defaultdict()
     data['sch:url'] = url
     article_root = bs.find('div', class_='entry-content content-article')
+    lang = detect(article_root.text)
+    if lang == 'en':
+        data['sch:availableLanguage'] = 'en'
     info_root = bs.find('div', class_='cs-entry__header-info')
     if article_root is not None:
-        if info_root is not None:
+        if info_root is None:
+            tei_logger.log('WARNING', f'{url}: META ROOT NOT FOUND!')
+        else:
             section_date_tag = info_root.find('div', class_='cs-entry__post-meta f16 meta-tiny')
             if section_date_tag is not None:
                 section_main = section_date_tag.find('a', class_='meta-categories')
@@ -58,9 +61,11 @@ def get_meta_from_articles_spec(tei_logger, url, bs):
             title = info_root.find('h1', class_='cs-entry__title')
             if title is not None:
                 article_title = title.find('span')
-                data['sch:name'] = article_title.text.strip()
+                if article_title is not None:
+                    data['sch:name'] = article_title.text.strip()
             else:
                 tei_logger.log('WARNING', f'{url}: TITLE NOT FOUND IN URL!')
+
         keywords_root = bs.find('ul', class_='post-categories')
         if keywords_root is not None:
             keywords_list = [t.text.strip() for t in keywords_root.find_all('a', class_='news-tag')]
@@ -69,23 +74,16 @@ def get_meta_from_articles_spec(tei_logger, url, bs):
         else:
             tei_logger.log('DEBUG', f'{url}: TAGS NOT FOUND!')
         # Problem: the sources of the articles are NOT handled in a standard manner at vadhajtasok.hu
-        # This routine tries to reach as many sources as possible in an automated manner but it can be improved.
-        source_root = bs.find_all('strong')
-        if len(source_root) == 1:
-            source_raw = source_root[0].text.strip()
-            # If there is only one element in source_root, we use SOURCE_1,
-            # because we would like to find only the source-related texts.
-            if any(src in source_raw for src in SOURCE_1):
-                source = source_raw.replace('Forrás: ', '').replace('Írta: ', '')
-                data['sch:source'] = source
-        elif len(source_root) > 1:
-            # If there are several elements in source_root, we use the latest one of them,
-            # because that's where the sources are stored in the majority of the cases.
+        # CHECK: The source extracting method had been simplified,
+        # because the previous solution gave us too many false authors.
+        source_root = article_root.find_all(recursive=False)
+
+        if len(source_root) > 0:
             source_raw = source_root[-1].text.strip()
-            # In this case we use both SOURCE_1 and SOURCE_2.
-            if any(src in source_raw for src in (SOURCE_1 + SOURCE_2)):
-                source = source_raw.replace('Forrás: ', '').replace('Írta: ', '')
-                data['sch:source'] = source
+            if len(source_raw) > 0 and len(source_raw.split()) < 6 and \
+                    (source_raw.startswith('Forrás:') or source_raw.startswith('Írta:') or
+                     source_raw in SOURCE_2):
+                data['originalAuthorString'] = [source_raw]
         else:
             tei_logger.log('DEBUG', f'{url}: SOURCE NOT FOUND!')
         return data
@@ -95,9 +93,6 @@ def get_meta_from_articles_spec(tei_logger, url, bs):
 
 
 def excluded_tags_spec(tag):
-    if tag.name not in HTML_BASICS:
-        tag.name = 'else'
-    tag.attrs = {}
     return tag
 
 
@@ -106,11 +101,7 @@ BIGRAM_RULES_SPEC = {}
 LINKS_SPEC = BASIC_LINK_ATTRS
 DECOMP = [(('script',), {})]
 
-MEDIA_LIST = [(('table',), {}),
-              (('figure',), {}),
-              (('img',), {}),
-              (('iframe',), {}),
-              (('div',), {'class': 'twitter-tweet twitter-tweet-rendered'})]
+MEDIA_LIST = []
 
 
 def decompose_spec(article_dec):
@@ -121,7 +112,7 @@ def decompose_spec(article_dec):
 BLACKLIST_SPEC = [url.strip() for url in open(
     os_path_join(os_path_dirname(os_path_abspath(__file__)), 'vadhajtasok_BLACKLIST.txt')).readlines()]
 
-LINK_FILTER_SUBSTRINGS_SPEC = re.compile('|'.join(['LINK_FILTER_DUMMY_STRING']))
+LINK_FILTER_SUBSTRINGS_SPEC = re.compile('|'.join(['http://%20Kov%C3%A1cs,%20Andr%C3%A1s%20%3Ckovacs.andras3@origo.']))
 
 MULTIPAGE_URL_END = re.compile(r'^\b$')  # Dummy
 
